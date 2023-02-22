0a5a613c0372f4745ff122fa4e4646b3116f456c
Seseok97 <112687040+Seseok97@users.noreply.github.com>
2023-02-22 10:51:27 +0900

    0222 수업 진행 내용 추가

--- Ex3_0221_class.java
+++ Ex3_0221_class.java
@@ -39,25 +39,125 @@ public class Ex3_0221_class {
 		// > 리턴값,파라미터가 없는 메서드 !!
 		ani.cry();
 	
-
-
+//20230222 이어서 !!
 		
 		
 		
 		
 		
 		
+		System.out.println("---------------------------------");
+		
+		//Animal_0221 클래스의 두번째 인스턴스(ani2) 생성
+		// 멤버변수 초기화 >> 이름: 냐옹이, 나이 : 2
+		// > cry() 메서드 호출
+		
+		Animal_0221 ani2 = new Animal_0221();
+		ani2.name = "야옹이";
+		ani2.age = 2;
+		System.out.println("이름: "+ani2.name);
+		System.out.println("나이: "+ani2.age+"살");
+		ani2.cry2();
+		
+		System.out.println("============================");
+		
+		//car class의 인스턴스 car 생성 및 멤버변수 초기화 후 출력
+		// 제조사: 현대
+		// 모델명: 그랜저
+		// 배기량: 3000
+		
+		Car_0221 car = new Car_0221();
+		System.out.println(car);
+		// Heap메모리 공간에 저장된 주소값 출력
+		
+		car.company = "현대";
+		car.modelName = "그랜저";
+		car.cc =3000;
+		System.out.println("제조사: "+car.company);
+		System.out.println("모델명: "+car.modelName);
+		System.out.println("배기량: "+car.cc);
+		//메서드 호출
+		car.speedUp();
+		car.speedDown();
+		
+		System.out.println("==========================");
+		//아래 student class 선언 보고 넘어오기
 		
 		
 		
 		
 		
+//		Student class 인스턴스 생성
+		// 이름: 홍길동
+		// 번호: 1
+		// 과목명: 백엔드 개발자과정
+		// >study() 메서드 호출하여 JAVA 문자열 전달
+		// >rest() 메서드 호출
+		
+		Student s1 = new Student();
+		s1.name="홍길동";
+		s1.id=1;
+		s1.className="백엔드 개발자 과정";
+		System.out.println("이름: "+s1.name);
+		System.out.println("번호: "+s1.id);
+		System.out.println("과목명: "+s1.className);
+		
+//		s1.study(null); // "null과목 공부중!!" 출력
+		s1.study("JAVA");
+		s1.rest();
+		
+		System.out.println("--------------------");
+		//Student class의 인스턴스 2 생성
+		// 자신의 정보를 통하여 메서드 호출
+		
+		Student myInfo = new Student();
+		myInfo.name="양세석";
+		myInfo.id=123;
+		myInfo.className="백엔드 개발자 과정";
+		System.out.println("이름: "+myInfo.name);
+		System.out.println("번호: "+myInfo.id);
+		System.out.println("과정명: "+myInfo.className);
+		
+		myInfo.study("JAVA");
+		myInfo.rest();
 		
 		
-		
-		
-		
+	
 		
 	}//main() method end
 
 }//Ex3 class end
+
+//하나의 자바파일 내에 여러개의 클래스를 정의할 수 있다 !!
+//단, 파일명과 동일한 클래스 외에는 public 키워드를 붙일수 없다.
+
+
+//학생(student) 클래스 정의
+//속성(멤버변수): 이름(name,String), 번호(id,int), 과정명(className,String)
+
+//기능(메서드): 1)공부(study()) - 리턴값 없음, 매개변수 - 과목명(subjectName,String)
+//								> "OOO 과목 공부중!" 출력
+//				2)휴실(rest()) - 리턴값 없음, 매개변수 없음
+//								> "휴식!" 출력
+
+//public class student{ //The public type student must be defined in its own file, 
+//					  //온파일에러, 이 클래스는 Ex3_0221_class의 것이기 때문에 발생하는 에러 
+//	
+//}
+
+class Student{ //err X
+	//멤버변수 선언
+	String name;
+	int id;
+	String className;
+	
+	//메서드 정의
+	public void study(String subjectName) {
+		//전달받은 인자값을 사용하여 출력
+		System.out.println(subjectName+"과목 공부중!!");
+	}//study() method end
+	public void rest() {
+		System.out.println("휴식!!");
+	}//rest() method end
+	
+}//Student class end
